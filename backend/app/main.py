2870ff85fa7fabe6fcf6d725daddebfdd6161366
Naomi Yoon <naomi.yoon.do@gmail.com>
2024-11-02 21:14:43 -0400

    Add test routes to API; Fix relative import errors in several files

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
-from .routers import fhir, dicom
+from routers import fhir, dicom
 
 app = FastAPI()
 
 app.include_router(fhir.router)
 app.include_router(dicom.router)
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+@app.get("/testroute")
+async def read_test():
+    return {"test": "test route exists"}
