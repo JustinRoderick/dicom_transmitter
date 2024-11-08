1d38bbd9a77fe32c885af1cc4fc3c395c58b8a89
Naomi Yoon <naomi.yoon.do@gmail.com>
2024-11-08 03:32:09 -0500

    Add DICOM routes

--- backend/app/api/dicom_handler.py
+++ backend/app/api/dicom_handler.py
@@ -8,3 +8,12 @@ def retrieve_dicom (accession_number: str):
     if response.status_code != 200:
         return {"error": "DICOM not found"}
     return response.content
+
+def query_orthanc_id_by_accession (accession_number: str):
+    return "0"
+
+def query_patient_resource_by_id (orthanc_id: str):
+    return {"ID": "0", "Type": "PatientPlaceholder"}
+
+def query_study_resource_by_id (orthanc_id: str):
+    return {"ID": "ABC", "Type": "StudyPlaceholder"}
--- backend/app/routers/dicom.py
+++ backend/app/routers/dicom.py
@@ -1,8 +1,22 @@
 from fastapi import APIRouter
-from api import dicom_handler
+from api.dicom_handler import *
 
 router = APIRouter()
 
-@router.get("/dicom/{accession_number}")
+@router.get("/{accession_number}")
 async def get_dicom(accession_number: str):
     return retrieve_dicom(accession_number)
+
+@router.get("/orthanc_id/{accession_number}")
+async def get_orthanc_id(accession_number: str):
+    return {"AccessionNumber": accession_number, 
+            "ID": query_orthanc_id_by_accession(accession_number)}
+
+@router.get("/patients/id/{orthanc_id}")
+async def get_patient_resource(orthanc_id: str):
+        return query_patient_resource_by_id(orthanc_id)
+
+@router.get("/studies/id/{orthanc_id}")
+async def get_study_resource(orthanc_id: str):
+        return query_study_resource_by_id(orthanc_id)
+
